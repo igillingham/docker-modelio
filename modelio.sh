e7786e0fc38f85376473c2465ccc1588e6cd28f0
Ian Gillingham <ian.gillingham@diamond.ac.uk>
2019-07-18 16:03:56 +0100

    Derive user id dynamically and pass to container

--- modelio.sh
+++ modelio.sh
@@ -2,12 +2,11 @@
 sudo docker run -ti --rm -e DISPLAY=$DISPLAY \
     --mount type=bind,source=/etc/machine-id,target=/etc/machine-id \
     --mount type=bind,source=/tmp/.X11-unix,target=/tmp/.X11-unix \
-    --mount type=bind,source=/home/ig43/.Xauthority,target=${HOME}/.Xauthority \
-    --mount type=bind,source=/home/ig43/modelio-projects/workspace,target=/home/developer \
+    --mount type=bind,source=${HOME}/modelio-projects/workspace,target=/home/developer \
     --name modelio \
     --net=host \
     --user $(id -u):$(id -g) \
     -e HOME=/home/developer \
-    modelio \
-    "$@"
+    -w /home/developer \
+    modelio
 
